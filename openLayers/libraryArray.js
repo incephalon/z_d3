f8767cab6ec19c8d1ed0f27a3b2bad47d9a153e8
robert <incephalon@gmail.com>
2014-04-16 10:03:37 -0500

    first d3 commit

---
+++ openLayers/libraryArray.js
@@ -0,0 +1,142 @@
+﻿var libraries=[
+  {
+      "library": 1,
+      "Latitude": 32.777733,
+      "Longitude": -96.797395
+  },
+  {
+      "library": 2,
+      "Latitude": 32.845576,
+      "Longitude": -96.787033
+  },
+  {
+      "library": 3,
+      "Latitude": 32.857186,
+      "Longitude": -96.756404
+  },
+  {
+      "library": 4,
+      "Latitude": 32.745073,
+      "Longitude": -96.826715
+  },
+  {
+      "library": 5,
+      "Latitude": 32.757236,
+      "Longitude": -96.911446
+  },
+  {
+      "library": 6,
+      "Latitude": 32.735008,
+      "Longitude": -96.887811
+  },
+  {
+      "library": 7,
+      "Latitude": 32.778642,
+      "Longitude": -96.859597
+  },
+  {
+      "library": 8,
+      "Latitude": 32.80996,
+      "Longitude": -96.752703
+  },
+  {
+      "library": 9,
+      "Latitude": 32.770364,
+      "Longitude": -96.766988
+  },
+  {
+      "library": 10,
+      "Latitude": 32.715132,
+      "Longitude": -96.790479
+  },
+  {
+      "library": 11,
+      "Latitude": 32.731187,
+      "Longitude": -96.683007
+  },
+  {
+      "library": 12,
+      "Latitude": 32.812025,
+      "Longitude": -96.8125
+  },
+  {
+      "library": 13,
+      "Latitude": 32.863915,
+      "Longitude": -96.856212
+  },
+  {
+      "library": 14,
+      "Latitude": 32.714031,
+      "Longitude": -96.857137
+  },
+  {
+      "library": 15,
+      "Latitude": 32.869739,
+      "Longitude": -96.770717
+  },
+  {
+      "library": 16,
+      "Latitude": 32.785429,
+      "Longitude": -96.699868
+  },
+  {
+      "library": 17,
+      "Latitude": 32.83591,
+      "Longitude": -96.672391
+  },
+  {
+      "library": 18,
+      "Latitude": 32.894796,
+      "Longitude": -96.811961
+  },
+  {
+      "library": 19,
+      "Latitude": 32.661434,
+      "Longitude": -96.841703
+  },
+  {
+      "library": 20,
+      "Latitude": 32.909725,
+      "Longitude": -96.863675
+  },
+  {
+      "library": 21,
+      "Latitude": 32.833781,
+      "Longitude": -96.859556
+  },
+  {
+      "library": 22,
+      "Latitude": 32.884867,
+      "Longitude": -96.717957
+  },
+  {
+      "library": 23,
+      "Latitude": 32.67369,
+      "Longitude": -96.760775
+  },
+  {
+      "library": 24,
+      "Latitude": 32.909248,
+      "Longitude": -96.74852
+  },
+  {
+      "library": 25,
+      "Latitude": 32.650014,
+      "Longitude": -96.966323
+  },
+  {
+      "library": 26,
+      "Latitude": 32.998175,
+      "Longitude": -96.795058
+  },
+  {
+      "library": 27,
+      "Latitude": 32.686344,
+      "Longitude": -96.6001
+  },
+  {
+      "library": 28,
+      "Latitude": 32.94995,
+      "Longitude": -96.783392
+  }
+];
